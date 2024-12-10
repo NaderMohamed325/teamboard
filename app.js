c7137580886b56aa4408f8e0b5a6a093ac3a7dbf
NaderMohamed325 <nadoorclasher@gmail.com>
2024-12-10 13:49:09 +0200

    "Project 100%"

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const userRouter = require('./routes/userRouter');
 
 const app = express();
-app.set('view engine', 'pug');
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.set('view engine', 'pug');
 app.use(express.static('views'));
 
 app.get('/', (req, res) => {
@@ -12,18 +14,6 @@ app.get('/', (req, res) => {
 
 app.use('/users', userRouter);
 
-app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
 
-app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: process.env.NODE_ENV === 'development' ? err : {}
-    });
-});
 
 module.exports = app;
